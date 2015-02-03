ba6b874c88c009ef1064ea9682f2443dc7147dc0
Alvin Morris <alvin.morris@gmail.com>
2015-02-03 11:55:39 -0500

    Finished exercise 07.

--- ex07/ex7.c
+++ ex07/ex7.c
@@ -16,5 +16,15 @@ int main(int argc, char *argv[])
     printf("You are expected to have %f bugs.\n",
             expected_bugs);
 
+    double part_of_universe = expected_bugs / universe_of_defects;
+    printf("That is only a %e portion of the universe.\n",
+            part_of_universe);
 
+    // This makes no sense, just a demo of something weird.
+    char nul_byte = '\0';
+    int care_percentage = bugs * nul_byte;
+    printf("Which means you shoud care %d%%.\n",
+            care_percentage);
+
+    return 0;
 }
