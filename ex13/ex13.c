70ffd4916019eba8b2cd8ee077eea0d3b08c407e
Alvin Morris <alvin.morris@gmail.com>
2015-02-23 20:28:05 -0500

    Finished exercise 13.

--- ex13/ex13.c
+++ ex13/ex13.c
@@ -28,7 +28,31 @@ int main(int argc, char *argv[])
 
             case 'i':
             case 'I':
-                printf("%d: 'I'\n
+                printf("%d: 'I'\n", i);
+                break;
+
+            case 'o':
+            case 'O':
+                printf("%d: 'O'\n", i);
+                break;
+
+            case 'u':
+            case 'U':
+                printf("%d: 'U'\n", i);
+                break;
+
+            case 'y':
+            case 'Y':
+                if(i<2)
+                {
+                    printf("%d: 'Y'\n", i);
+                }
+                break;
+
+            default:
+                printf("%d: %c is not a vowel\n", i, letter);
         }
     }
+
+    return 0;
 }
