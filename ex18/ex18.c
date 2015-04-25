43e5fb814cf2b7ef4fd26055e12a8c31efcc7b3d
Alvin Morris <alvin@adm-fedmb.home>
2015-04-24 22:16:15 -0400

    Finished exercise 18

--- ex18/ex18.c
+++ ex18/ex18.c
@@ -23,7 +23,7 @@ void die(const char *message)
 typedef int (*compare_cb)(int a, int b);
 
 /**
- * A classic bubble sor function that uses the
+ * A classic bubble sort function that uses the
  * compare_cb to do the sorting.
  */
 int *bubble_sort(int *numbers, int count, compare_cb cmp)
@@ -77,5 +77,37 @@ int strange_order(int a, int b)
  */
 void test_sorting(int *numbers, int count, compare_cb cmp)
 {
+    int i = 0;
+    int *sorted = bubble_sort(numbers, count, cmp);
 
+    if(!sorted) die("Failed to sort as requested.");
+
+    for(i = 0; i < count; i++)
+        printf("%d ", sorted[i]);
+    printf("\n");
+
+    free(sorted);
+}
+
+int main(int argc, char *argv[])
+{
+    if(argc < 2) die("USAGE: ex18 4 3 1 5 6");
+
+    int count = argc - 1;
+    int i = 0;
+    char **inputs = argv + 1;
+
+    int *numbers = malloc(count * sizeof(int));
+    if(!numbers) die("Memory error.");
+
+    for(i = 0; i < count; i++)
+        numbers[i] = atoi(inputs[i]);
+
+    test_sorting(numbers, count, sorted_order);
+    test_sorting(numbers, count, reverse_order);
+    test_sorting(numbers, count, strange_order);
+
+    free(numbers);
+
+    return 0;
 }
